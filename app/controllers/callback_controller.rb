dae908924eb11701ff7fdcb49ce8fb6d252cbbc7
kouuuki <mark.web.888@gmail.com>
2018-01-30 00:58:35 +0900

    add

--- app/controllers/callback_controller.rb
+++ app/controllers/callback_controller.rb
@@ -1,9 +1,10 @@
 class CallbackController < ApplicationController
   def callback
-    # if params["hub.verify_token"] == "hogehoge"
-    #   render json: params["hub.challenge"]
-    # else
-    #   render json: "Error, wrong validation token"
-    # end
+    if params["hub.verify_token"] == "hogehoge"
+      # render json: params["hub.challenge"]
+      render json: "Error, wrong validation token"
+    else
+      render json: "Error, wrong validation token"
+    end
   end
 end
