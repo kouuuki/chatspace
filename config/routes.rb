295fbf7fa7b9455e63b455616fed7f434cff0988
kouuuki <mark.web.888@gmail.com>
2018-01-29 22:00:21 +0900

    add callback

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,7 @@ Rails.application.routes.draw do
 
   get '/users/search/:name', to: 'users#search'
   root 'groups#index'
+
+  get '/api/callback', to: 'callbacks#callback'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
