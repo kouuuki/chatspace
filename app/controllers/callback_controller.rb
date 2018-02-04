1134b66995541d308e5090a6ea1099ad87672335
kouuuki <mark.web.888@gmail.com>
2018-02-04 21:15:01 +0900

    bind

--- app/controllers/callback_controller.rb
+++ app/controllers/callback_controller.rb
@@ -8,14 +8,9 @@ class CallbackController < ApplicationController
   # end
 
   def callback
-    binding.pry
     token = "EAAOucPomWPoBAFMahDF2U3HGwaHdp1ZCmZAbh9WUFjQ9jMWK1W9cz42NqrUNoV4LOy7mfmHPTiyPnvicjerOCabmUzlOEhjvzKkwE2Szq0lyBKtfCIks8NlZAFkTWPP6WaheYelm7iEk6hyYw6qoXsyFNe0GktxckSZBAZBRrL9dEY3cvnEnXldMiA4Tq0eoZD"
 
     message = params["entry"][0]["messaging"][0]
-    # p params
-    # p message
-    # puts "テストです"
-
 
 
     if message.include?("message")
@@ -40,8 +35,49 @@ class CallbackController < ApplicationController
       }
     else
       #botの発言
-      return text
-      p "こんにちは"
+      # text
+      # p "こんにちは"
+      if text == "天気"
+        button_structured_message_request_body(sender, "いつの天気？", *weather_buttons)
+      end
     end
   end
+
+  def button_structured_message_request_body(sender, text, *buttons)
+  {
+    recipient: {
+      id: sender
+    },
+    message: {
+      attachment: {
+        type: "template",
+        payload: {
+          template_type: "button",
+          text: text,
+          buttons: buttons
+        }
+      }
+    }
+  }.to_json
+end
+
+def weather_buttons
+  [
+    {
+      type: "postback",
+      title: "今日",
+      payload: "today_weather"
+    },
+    {
+      type: "postback",
+      title: "明日",
+      payload: "tomorrow_weather"
+    },
+    {
+      type: "web_url",
+      url: "http://www.jma.go.jp/jp/week/319.html",
+      title: "その他"
+    }
+  ]
+end
 end
