2a8efec7abc6a9b0f419b0539c46e0414c97068e
kouuuki <mark.web.888@gmail.com>
2018-02-04 20:26:39 +0900

    bind

--- app/controllers/callback_controller.rb
+++ app/controllers/callback_controller.rb
@@ -12,30 +12,33 @@ class CallbackController < ApplicationController
     token = "EAAOucPomWPoBALBWyWKbNT9bhICjg7eq039DW0HpGqGOVE53ShnVax95K08mqeMVzUW8NlJsbEZAnHSJ4ZBKOZCKxFZC6iomcZBwW8kcaoE7Rf8ta6Hi5Lp9DARGhK3lwpXhcO9dlhZCNuAyVp84UbciZA4QCZCvlxiTlm5vz8j5cSQWQ1PxFEZAoZBbPgAZCnFuogZD"
 
     message = params["entry"][0]["messaging"][0]
+    return message["message"]["text"]
 
-    if message.include?("message")
+    
 
-      #ユーザーの発言
-
-      sender = message["sender"]["id"]
-      text = message["message"]["text"]
-
-      endpoint_uri = "https://graph.facebook.com/v2.6/me/messages?access_token=" + token
-      request_content = {recipient: {id:sender},
-                         message: {text: text}
-                        }
-
-      content_json = request_content.to_json
-      RestClient.post(endpoint_uri, content_json, {
-        'Content-Type' => 'application/json; charset=UTF-8'
-      }){ |response, request, result, &block|
-        p response
-        p request
-        p result
-      }
-    else
-      #botの発言
-      return text
-    end
+    # if message.include?("message")
+    #
+    #   #ユーザーの発言
+    #
+    #   sender = message["sender"]["id"]
+    #   text = message["message"]["text"]
+    #
+    #   endpoint_uri = "https://graph.facebook.com/v2.6/me/messages?access_token=" + token
+    #   request_content = {recipient: {id:sender},
+    #                      message: {text: text}
+    #                     }
+    #
+    #   content_json = request_content.to_json
+    #   RestClient.post(endpoint_uri, content_json, {
+    #     'Content-Type' => 'application/json; charset=UTF-8'
+    #   }){ |response, request, result, &block|
+    #     p response
+    #     p request
+    #     p result
+    #   }
+    # else
+    #   #botの発言
+    #   return text
+    # end
   end
 end
