f61e1b83ec750bd4286b4fe0bf75f65996fdf7c0
kouuuki <mark.web.888@gmail.com>
2018-02-08 22:38:37 +0900

    bind

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,9 +1,9 @@
 class Group < ApplicationRecord
-    has_many :users, through: :members
-    has_many :members, dependent: :destroy
-    has_many :messages, dependent: :destroy
+    # has_many :users, through: :members
+    # has_many :members, dependent: :destroy
+    # has_many :messages, dependent: :destroy
 
-    accepts_nested_attributes_for :messages, allow_destroy: true
+    # accepts_nested_attributes_for :messages, allow_destroy: true
 
     validates :name, presence: true
     validates :name, uniqueness: true
